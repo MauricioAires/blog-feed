cbf659385b72bbeb6639e580018f6244a4f6d7fe
MauricioAires <mauricioaires015@gmail.com>
2022-12-30 15:58:04 -0300

    feat: sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react'
+
+import S from './Sidebar.module.css'
+
+export function Sidebar() {
+  return (
+    <aside className={S.sidebar}>
+      <img
+        className={S.cover}
+        src="https://images.unsplash.com/photo-1635830625698-3b9bd74671ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
+        alt="User cover"
+      />
+
+      <div className={S.profile}>
+        <img
+          className={S.avatar}
+          src="https://github.com/MauricioAires.png"
+          alt="Mauricio Aires"
+        />
+        <strong>Mauricio Aires</strong>
+        <span>Frontend PL</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} /> Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
