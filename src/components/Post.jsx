00eec5b91b91f22cfe0331d66e942e852c8b20e7
MauricioAires <mauricioaires015@gmail.com>
2022-12-30 17:39:35 -0300

    feat: add form comment

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -35,6 +35,15 @@ export function Post() {
           <a href="#">#rocketseat</a>
         </p>
       </div>
+
+      <form className={S.commentForm}>
+        <strong> Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
