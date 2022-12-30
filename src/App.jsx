588bc5519a95ae442ab51862d7fdd5997ced37f3
MauricioAires <mauricioaires015@gmail.com>
2022-12-30 17:01:24 -0300

    feat: content

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,23 @@
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Sidebar } from './components/Sidebar'
+import { Post } from './components/Post'
 
 import './styles/global.css'
 import S from './App.module.css'
-import { Sidebar } from './components/Sidebar'
 
 export function App() {
   return (
-    <div>
+    <div
+      style={{
+        position: 'relative'
+      }}
+    >
       <Header />
       <div className={S.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Mauricio Aires"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem possimus placeat fuga omnis, quasi tenetur. Iusto, pariatur repellendus? A corporis quibusdam molestiae assumenda voluptatem quo alias cupiditate iure vitae? Perspiciatis!"
-          />
-
-          <Post
-            author="Alex Alan"
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia, provident! Perspiciatis ducimus quis excepturi officia quasi cum, autem placeat laborum natus laudantium quia totam at doloremque, nulla a consectetur soluta."
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
---
+++ src/components/Post.jsx
@@ -0,0 +1,40 @@
+import S from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={S.post}>
+      <header>
+        <div className={S.author}>
+          <img className={S.avatar} src="https://github.com/diego3g.png" />
+          <div className={S.authorInfo}>
+            <strong>Diego Fernandes</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:30">
+          Públicado há 1h
+        </time>
+      </header>
+
+      <div className={S.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p>
+          👉 <a href="#">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  )
+}
